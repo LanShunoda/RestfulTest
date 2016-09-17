ca343003170dfe8f04aa3ac1b3ca12db2af96c66
Plorial <lanshunoda@rambler.ru>
2016-09-17 23:30:15 +0300

    added filter

--- src/main/java/com/plorial/restfultest/ContactsController.java
+++ src/main/java/com/plorial/restfultest/ContactsController.java
@@ -2,8 +2,12 @@ package com.plorial.restfultest;
 
 import com.plorial.restfultest.pojo.Contact;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -12,10 +16,35 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class ContactsController {
 
-    private final AtomicLong counter = new AtomicLong();
+    static List<Contact> contacts;
+
+    static {
+        initContacts();
+    }
 
     @RequestMapping("/hello/contact")
     public Contact contact() {
-        return new Contact(counter.incrementAndGet(), "Name");
+        return new Contact(0 , "Name");
+    }
+
+    @RequestMapping("/hello/contacts")
+    public Contact[] contacts(@RequestParam(value="nameFilter", required=false, defaultValue = "")String nameFilter) {
+        return filterContacts(contacts, nameFilter);
+    }
+
+    private static Contact[] filterContacts(List<Contact> contacts, String regex) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if(contacts.get(i).getName().matches(regex)){
+                contacts.remove(i);
+            }
+        }
+        initContacts();
+        return contacts.toArray(new Contact[0]);
+    }
+
+    private static void initContacts(){
+        contacts = new LinkedList<>();
+        contacts.add(new Contact(1, "1"));
+        contacts.add(new Contact(2, "2"));
     }
 }
