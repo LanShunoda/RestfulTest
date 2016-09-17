f0b78cbd9bd5db557f6cff00a3ec18b0722a373d
Plorial <lanshunoda@rambler.ru>
2016-09-18 00:03:11 +0300

    added bd

--- src/main/java/com/plorial/restfultest/ContactsController.java
+++ src/main/java/com/plorial/restfultest/ContactsController.java
@@ -1,14 +1,14 @@
 package com.plorial.restfultest;
 
 import com.plorial.restfultest.pojo.Contact;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by plorial on 9/17/16.
@@ -16,7 +16,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class ContactsController {
 
-    static List<Contact> contacts;
+    private static JdbcTemplate jdbcTemplate;
 
     static {
         initContacts();
@@ -29,7 +29,7 @@ public class ContactsController {
 
     @RequestMapping("/hello/contacts")
     public Contact[] contacts(@RequestParam(value="nameFilter", required=false, defaultValue = "")String nameFilter) {
-        return filterContacts(contacts, nameFilter);
+        return filterContacts(getAllContactsFromDB(), nameFilter);
     }
 
     private static Contact[] filterContacts(List<Contact> contacts, String regex) {
@@ -38,13 +38,36 @@ public class ContactsController {
                 contacts.remove(i);
             }
         }
-        initContacts();
         return contacts.toArray(new Contact[0]);
     }
 
     private static void initContacts(){
-        contacts = new LinkedList<>();
+        List<Contact> contacts = new ArrayList<>();
         contacts.add(new Contact(1, "1"));
         contacts.add(new Contact(2, "2"));
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setUrl("jdbc:h2:~/name");
+        dataSource.setPassword("");
+
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table contacts if exists");
+        jdbcTemplate.execute("create table contacts(" + "id serial, name varchar(255))");
+        for (Contact c : contacts){
+            jdbcTemplate.update("INSERT INTO contacts(id,name) values(?,?)",c.getId(), c.getName());
+        }
+    }
+
+    public List<Contact> getAllContactsFromDB() {
+        List<Contact> results = jdbcTemplate.query(
+                "select * from contacts",
+                (rs, rowNum) -> {
+                    return new Contact(rs.getLong("id"), rs.getString("name"));
+                });
+        return results;
     }
 }
